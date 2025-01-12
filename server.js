f39851f26d1d6591e7c21d2b3d84977daa89e2d3
hammad0860 <muhammad.h0860@gmail.com>
2025-01-12 15:30:32 -0500

    mac installer should be working

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.post('/api/build-installer', (req, res) => {
 
     const platform = os.platform();
 
+    // Executes the installer based for Windows
     if (platform == 'win32') {
         const installerPath = path.join(electronAppPath, 'out', 'make', 'squirrel.windows', 'x64');
         const files = fs.readdirSync(installerPath);
@@ -28,15 +29,57 @@ app.post('/api/build-installer', (req, res) => {
         
         execFile(path.join(installerPath, setupFile), (error, stdout, stderr) => {
             if (error) {
-            console.error(`Error executing installer: ${error.message}`);
-            return res.status(500).json({ message: 'Failed to execute installer.' });
+            console.error(`Error executing Windows installer: ${error.message}`);
+            return res.status(500).json({ message: 'Failed to execute Windows installer.' });
             }
             if (stderr) {
             console.error(`stderr: ${stderr}`);
             }
-            return res.status(200).json({ message: 'Installer executed successfully.' });
-            
+            return res.status(200).json({ message: 'Windows Installer executed successfully.' });
+
         })
+    }
+
+    // Executes the installer based for Mac
+    else if (platform === 'darwin') {
+        const installerPath = path.join(electronAppPath, 'out', 'make', 'dmg');
+        const files = fs.readdirSync(installerPath);
+        const dmgFile = files.find(file => file.endsWith('.dmg'));
+    
+       
+        execFile('open', [path.join(installerPath, dmgFile)], (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing Mac installer: ${error.message}`);
+                return res.status(500).json({ message: 'Failed to execute Mac installer.' });
+            }
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+            }
+            console.log('Mac installer executed successfully.');
+            return res.status(200).json({ message: 'Mac installer executed successfully.' });
+        });
+    }
+
+    if (platform === 'linux') {
+        const installerPath = path.join(electronAppPath, 'out', 'make', 'appimage'); 
+        const files = fs.readdirSync(installerPath);
+        const appImageFile = files.find(file => file.endsWith('.AppImage')); 
+    
+
+        const appImageFullPath = path.join(installerPath, appImageFile);
+        fs.chmodSync(appImageFullPath, '755');
+    
+        execFile(appImageFullPath, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing AppImage: ${error.message}`);
+                return res.status(500).json({ message: 'Failed to execute AppImage.' });
+            }
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+            }
+            console.log('AppImage executed successfully.');
+            return res.status(200).json({ message: 'AppImage executed successfully.' });
+        });
     }
 
 });
